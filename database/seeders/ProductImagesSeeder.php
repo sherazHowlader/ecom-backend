19935bbfcab86032dc93ed19d40bee0032c3b71e
sherazHowlader <mdshiraj72@gmail.com>
2023-02-26 15:49:52 +0600

    Save token after login success

--- database/seeders/ProductImagesSeeder.php
+++ database/seeders/ProductImagesSeeder.php
@@ -18,50 +18,50 @@ class ProductImagesSeeder extends Seeder
     {
         ProductImages::updateOrCreate([
             'product_id' => 1,
-            'image'      => '/frontend/images/product-details/01.jpg',
+            'image'      => 'frontend/images/product-details/01.jpg',
         ]);
 
         ProductImages::updateOrCreate([
             'product_id' => 1,
-            'image'      => '/frontend/images/product-details/02.jpg',
+            'image'      => 'frontend/images/product-details/02.jpg',
         ]);
 
         ProductImages::updateOrCreate([
             'product_id' => 1,
-            'image'      => '/frontend/images/product-details/03.jpg',
+            'image'      => 'frontend/images/product-details/03.jpg',
         ]);
 
         ProductImages::updateOrCreate([
             'product_id' => 1,
-            'image'      => '/frontend/images/product-details/04.jpg',
+            'image'      => 'frontend/images/product-details/04.jpg',
         ]);
 
         ProductImages::updateOrCreate([
             'product_id' => 1,
-            'image'      => '/frontend/images/product-details/05.jpg',
+            'image'      => 'frontend/images/product-details/05.jpg',
         ]);
 
 
         ProductImages::updateOrCreate([
             'product_id' => 4,
-            'image'      => '/frontend/images/product-details/06.jpg',
+            'image'      => 'frontend/images/product-details/06.jpg',
         ]);
         ProductImages::updateOrCreate([
             'product_id' => 4,
-            'image'      => '/frontend/images/product-details/07.jpg',
+            'image'      => 'frontend/images/product-details/07.jpg',
         ]);
         ProductImages::updateOrCreate([
             'product_id' => 4,
-            'image'      => '/frontend/images/product-details/08.jpg',
+            'image'      => 'frontend/images/product-details/08.jpg',
         ]);
 
         ProductImages::updateOrCreate([
             'product_id' => 3,
-            'image'      => '/frontend/images/product-details/09.jpg',
+            'image'      => 'frontend/images/product-details/09.jpg',
         ]);
         ProductImages::updateOrCreate([
             'product_id' => 3,
-            'image'      => '/frontend/images/product-details/10.jpg',
+            'image'      => 'frontend/images/product-details/10.jpg',
         ]);
     }
 }
