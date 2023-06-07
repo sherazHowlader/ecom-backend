150b8fd4f3f68d5ce52a8435882b5dcbc55625a6
sherazhowlader <mdshiraj72@gmail.com>
2023-06-07 11:30:30 +0600

    add icon

--- resources/views/partials/app.blade.php
+++ resources/views/partials/app.blade.php
@@ -7,7 +7,7 @@
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" href="https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/3_avatar-512.png" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('images/logo.png')}}" type="image/x-icon">
     <title> @yield('title') - {{env('APP_NAME')}} </title>
 
     <!-- Bootstrap Library -->
